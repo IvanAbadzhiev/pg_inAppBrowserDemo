f8116d535f236f0814f941fb9ac9d05c3f033d2e
ivan3a <email9410@abv.bg>
2015-07-23 11:17:33 +0300

    dasd

--- www/js/index.js
+++ www/js/index.js
@@ -45,7 +45,7 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-        window.open('http://www.kidzout.com', '_system');
+        navigator.app.loadUrl('https://google.com/', { openExternal:true });
     }
 };
 
